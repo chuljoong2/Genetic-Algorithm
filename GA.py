17a92e669435195737654b419590f708c3d32674
윤철중 <cjfwnd123zz@gmail.com>
2022-06-17 18:19:52 +0900

    add: 반복 수행 및 결과 분석 추가

--- GA.py
+++ GA.py
@@ -74,33 +74,31 @@ def init_population():
 #  평균 제곱 오차 구하기
 def get_mean_squared_errores(p):
     mean_squared_errores = []
-    for i in range(population_length):
-        w = p[i][0]  # 기울기
-        b = p[i][1]  # y 절편
+    for m in range(population_length):
+        w = p[m][0]  # 기울기
+        b = p[m][1]  # y 절편
 
-        sum_squared_error = 0
-        for score in scores:
-            rs, ws = score[0], score[1]  # reading_score, writing_score
+        sse = 0
+        for reading, writing in scores:
+            p_ws = w * reading + b
+            se = (writing - p_ws) ** 2  # (실제값 - 예측값)의 제곱
+            sse += se
 
-            predicted_ws = w * rs + b
-            squared_error = (ws - predicted_ws) ** 2  # (실제값 - 예측값)의 제곱
-            sum_squared_error += squared_error
-
-        mean_squared_error = sum_squared_error / total_scores_length  # 평균 제곱 오차
+        mean_squared_error = sse / total_scores_length  # 평균 제곱 오차
         mean_squared_errores.append(mean_squared_error)
 
     return mean_squared_errores
 
 
 # 적합도 구하기
-def get_fittnesses(msnes):
+def get_fittnesses(mean_squared_errores):
     fittnesses = []
-    sum_msnes = sum(msnes)
+    sum_mean_squared_errores = sum(mean_squared_errores)
 
     # 평균 제곱 오차가 낮을 수록 적합도가 높기 때문에
     # (1 - 평균 제곱 오차 비율)을 통해서 평균 제곱 오차가 낮을 수록 적합도를 높게 설정
-    for msn in msnes:
-        fittnesses.append(1-msn/sum_msnes)
+    for mse in mean_squared_errores:
+        fittnesses.append(sum_mean_squared_errores/mse)
 
     return fittnesses
 
@@ -128,9 +126,9 @@ def selection(p):
     # 룰렛 휠 방식
     for _ in range(population_length):
         random_percentage = random.random()
-        for i in range(population_length):
-            if random_percentage < percenatages[i]:
-                selected_p.append(p[i])
+        for k in range(population_length):
+            if random_percentage < percenatages[k]:
+                selected_p.append(p[k])
                 break
 
     return selected_p
@@ -140,10 +138,10 @@ def selection(p):
 def crossover(p):
     crossover_p = []
     p.sort()  # 같은 직선끼리 교차할 확률을 줄이기 위함
-    for i in range(population_length//2):
-        crossover_p.append([(p[i][0] + p[i + population_length // 2][0])/2,
-                            (p[i][1] + p[i + population_length // 2][1])/2])
-        crossover_p.append([(p[i][0] + p[-1-i][0])/2, (p[i][1] + p[-1-i][1])/2])
+    for i in range(population_length // 2):
+        crossover_p.append([(p[i][0] + p[i + population_length // 2][0]) / 2,
+                            (p[i][1] + p[i + population_length // 2][1]) / 2])
+        crossover_p.append([(p[i][0] + p[-1 - i][0]) / 2, (p[i][1] + p[-1 - i][1]) / 2])
 
     return crossover_p
 
@@ -151,19 +149,15 @@ def crossover(p):
 # 돌연변이 연산
 def mutation(p):
     mutated_p = []
-    for i in range(population_length):
+    for l in range(population_length):
         random_percentage = random.random()
-        # 돌연변이가 일어날 확률 1%
-        if random_percentage > 0.975:
-            mutated_p.append([p[i][0], p[i][1]-0.5])
-        elif random_percentage > 0.95:
-            mutated_p.append([p[i][0], p[i][1]+0.5])
-        elif random_percentage > 0.925:
-            mutated_p.append([p[i][0]-0.1, p[i][1]])
-        elif random_percentage > 0.9:
-            mutated_p.append([p[i][0]+0.1, p[i][1]])
+        # 돌연변이가 일어날 확률 5%
+        if random_percentage > 0.5:
+            mutated_p.append([p[l][0]+0.1, p[l][1]-0.7])
+        elif random_percentage > 0.0:
+            mutated_p.append([p[l][0]-0.1, p[l][1]+0.7])
         else:
-            mutated_p.append([p[i][0], p[i][1]])
+            mutated_p.append([p[l][0], p[l][1]])
 
     return mutated_p
 
@@ -183,10 +177,83 @@ total_scores_length = len(scores)
 population = init_population()
 print_scatterplot(population)
 
-while generation < 100:
+result = []
+est_gradient, est_intercept = 0, 0
+while generation < 10:
     generation += 1
+    est_mses = get_mean_squared_errores(population)
+
+    # 평균 제곱 오차의 평균
+    est_sum_mses = sum(est_mses)
+    est_avg_mses = est_sum_mses / population_length
+    result.append([generation, est_avg_mses])
+
     selected_population = selection(population)
     crossover_population = crossover(selected_population)
     mutated_population = mutation(crossover_population)
     population = mutated_population
+
+    temp = []
+    for t in range(population_length):
+        temp.append([est_mses[t], t])
+
+    # 마지막 세대의 최적해가 담김 (최종 추정된 회귀식)
+    est_gradient = population[min(temp)[1]][0]
+    est_intercept = population[min(temp)[1]][1]
+
     print_scatterplot(population)
+
+#  라이브러리로 찿은 회귀식
+opt_sum_sse, est_sum_sse = 0, 0
+for score in scores:
+    rs, ws = score[0], score[1]  # reading_score, writing_score
+
+    predicted_ws = regression_gradient * rs + regression_intercept
+    opt_se = (ws - predicted_ws) ** 2  # (실제값 - 예측값)의 제곱
+    opt_sum_sse += opt_se
+    predicted_ws = est_gradient * rs + est_intercept
+    est_se = (ws - predicted_ws) ** 2  # (실제값 - 예측값)의 제곱
+    est_sum_sse += est_se
+
+# 평균 제곱 오차
+opt_mse = opt_sum_sse / total_scores_length
+est_mse = est_sum_sse / total_scores_length
+
+fig = plt.figure(figsize=(8, 8))
+fig.set_facecolor('white')
+
+font_size = 15
+plt.title("Mean MSE change", fontsize=font_size)
+for i in range(len(result)):
+    plt.scatter(result[i][0], result[i][1])
+
+x = np.arange(0, generation)  # x의 범위 0 ~ generaion
+
+plt.hlines(opt_mse, 0, 30, color='red', linestyle='solid', linewidth=2)
+plt.xlabel('generaion', fontsize=font_size)
+plt.ylabel('MSE', fontsize=font_size)
+plt.xlim([0, 10])
+plt.ylim([0, 100])
+
+plt.show()
+
+
+# 최종 추정회귀식과 회귀식 비교
+fig = plt.figure(figsize=(8, 8))
+fig.set_facecolor('white')
+
+font_size = 15
+plt.title("estimated regression comapre", fontsize=font_size)
+plt.scatter(df['reading_score'], df['writing_score'])
+x = np.arange(0, 100)
+
+plt.plot(x, regression_gradient * x + regression_intercept, color="red", linewidth="2.5")
+plt.plot(x, est_gradient * x + est_intercept, color="orange", linestyle="--", linewidth="2.5")
+plt.xlabel('Reading Score', fontsize=font_size)
+plt.ylabel('Writing Score', fontsize=font_size)
+plt.xlim([0, 100])
+plt.ylim([0, 100])
+
+plt.show()
+
+
